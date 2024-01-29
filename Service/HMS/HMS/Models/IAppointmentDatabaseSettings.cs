5911799ac9b067de0bd37ffe2fd9a974bcdf1279
miranrexhepi <44025690+MiranRexhepi@users.noreply.github.com>
2024-01-29 18:51:37 +0100

    Findacarrier login, jobs Work in progress

--- Service/HMS/HMS/Models/IAppointmentDatabaseSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HMS.Models
-{
-    public interface IAppointmentDatabaseSettings
-    {
-        string AppointmentHMSCollectionName { get; set; }
-        string ConnectionString { get; set; }
-        string DatabaseName { get; set; }
-    }
-}
